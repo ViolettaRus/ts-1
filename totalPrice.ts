65092a5c10349a9a552a0e55acd6935d75a76c79
violetta <violetta.rusovich.2002@gmail.com>
2025-10-30 20:06:31 +0300

    refactor(totalPrice): use utils/constants and export const function

---
+++ totalPrice.ts
@@ -0,0 +1,22 @@
+import { TotalPriceParams } from "./totalPrice.types"
+import { applyDiscount, assertValidDiscount, assertValidMonths, assertValidPrice, roundToCents } from "./pricing.utils"
+
+export const totalPrice = (params: TotalPriceParams) => {
+  const { price, discount = 0 } = params;
+
+  assertValidPrice(price);
+  assertValidDiscount(discount);
+
+  const discountedTotal = applyDiscount(price, discount);
+
+  if (params.isInstallment) {
+    const { months } = params;
+    assertValidMonths(months);
+    return roundToCents(discountedTotal / months);
+  }
+
+  return roundToCents(discountedTotal);
+}
+
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+console.log(price); // 6250
